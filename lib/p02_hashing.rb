6c89c9c7354169d0904d9f85938a70eb947f3bbb
Calvin <calvinhlee23@gmail.com>
2016-11-29 18:14:22 -0800

    done with ph3

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,13 @@ end
 
 class Array
   def hash
+    self.inject(0) { |sum, i| sum + (i.to_s.ord^sum) }
   end
 end
 
 class String
   def hash
+    self.split("").map{|ch| ch.ord}.inject(0) { |sum, i| sum + (i^sum) }
   end
 end
 
@@ -16,5 +18,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
+    self.keys.sort.inject(0) { |sum, i| sum + (i.to_s.ord)^sum }
   end
 end
--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -4,15 +4,30 @@ class HashSet
   attr_reader :count
 
   def initialize(num_buckets = 8)
+    @count = 0
+    @store = Array.new(num_buckets, [])
   end
 
   def insert(key)
+    resize! if @count >= num_buckets
+    hash = key.hash
+    ind = hash % num_buckets
+    @store[ind].push(key);
+    @count +=1
   end
 
   def include?(key)
+    hash = key.hash
+    ind = hash % num_buckets
+    @store[ind].each{|el| return true if el == key}
+    false
   end
 
   def remove(key)
+    hash = key.hash
+    ind = hash % num_buckets
+    @store[ind].delete(key)
+    @count -= 1
   end
 
   private
@@ -22,8 +37,20 @@ class HashSet
   end
 
   def num_buckets
+    @store.length
   end
 
   def resize!
+    newSize = num_buckets*2
+    newStore = Array.new(newSize, []);
+    @store.each do |el|
+      el.each do |value|
+        newInd = value % newSize
+        newStore[newInd].push(value)
+      end
+    end
+
+    @store = newStore
   end
+
 end
