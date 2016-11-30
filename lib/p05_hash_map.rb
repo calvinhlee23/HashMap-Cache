bf3e8f2d466f1cc29ffdecf8287c37426c3f6b0c
Calvin <calvinhlee23@gmail.com>
2016-11-29 20:35:00 -0800

    ph5 done

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -1,25 +1,45 @@
 require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
+require "byebug"
 include Enumerable
 class HashMap
   attr_reader :count
 
   def initialize(num_buckets = 8)
+    @count = 0
+    @store = Array.new(num_buckets, LinkedList.new);
   end
 
   def include?(key)
+    ind = key.hash % num_buckets
+    @store[ind].include?(key)
   end
 
   def set(key, val)
+    ind = key.hash % num_buckets
+    if self.include?(key)
+      @store[ind].getLink(key).val = val
+      return true
+    end
+    resize! if count >= num_buckets
+    @store[ind].insert(key, val)
+    @count +=1
+    true
   end
 
   def get(key)
+    ind = key.hash % num_buckets
+    @store[ind].get(key)
   end
 
   def delete(key)
+    ind = key.hash % num_buckets
+    @count -= 1
+    @store[ind].remove(key)
   end
 
   def each
+
   end
 
   # uncomment when you have Enumerable included
@@ -36,9 +56,19 @@ class HashMap
   private
 
   def num_buckets
+    @store.length
   end
 
   def resize!
+    newSize = num_buckets*2
+    newStore = Array.new(newSize, LinkedList.new)
+    @store.each do |list|
+      list.each do |node|
+        ind = node.key.hash % newSize
+        newStore[ind].insert(node.key, node.val)
+      end
+    end
+    @store = newStore
   end
 
   def bucket(key)
